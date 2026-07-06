935a2011d5c3c40d7f99ec24b3f3f52785968857
Grayson Hawkins <graysonhawkins17@gmail.com>
2026-07-06 02:10:15 -0400

    Set up PostgreSQL and Prisma

---
+++ server/prisma/migrations/20260706060755_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" SERIAL NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "description" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
